db4427c2dc8f2b347691e899129d58939e5680d8
Ramoxa <ramoxa0007@yandex.ru>
2024-04-05 01:08:33 +0300

    V.1

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -8,7 +8,6 @@ import ru.yandex.practicum.filmorate.storage.FilmStorage;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class FilmService {
